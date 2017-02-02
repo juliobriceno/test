df783bc47630fdf589590ef8346e61eac5b2bd2c
Julio Briceno <juliob@ptree.com.mx>
2017-02-01 20:55:02 -0600

    'Ejemplo3'

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app     = express();
+
+app.use("/", express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
